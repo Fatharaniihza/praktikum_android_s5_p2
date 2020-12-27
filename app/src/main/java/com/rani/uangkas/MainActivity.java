9882a9cbf455de8947eccf143b0ab4a92df82820
Fatharani Ihza <fatharaniiihza@gmail.com>
2020-12-27 22:46:44 +0800

    Pertemuan Ke 3
    Signed-off-by: Fatharani Ihza <fatharaniiihza@gmail.com>

--- app/src/main/java/com/rani/uangkas/MainActivity.java
+++ app/src/main/java/com/rani/uangkas/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rani.uangkas;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -13,13 +16,37 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.rani.uangkas.databinding.ActivityMainBinding;
+import com.rani.uangkas.helper.SqliteHelper;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static TextView text_filter;
+    public static String transaksi_id, tgl_dari, tgl_ke;
+    public static boolean filter;
+    TextView text_masuk, text_keluar, text_total;
+    ListView list_kas;
+    SwipeRefreshLayout swipe_refresh;
+    ArrayList<HashMap<String, String>> aruskas = new ArrayList<HashMap<String, String>>();
+    String query_kas, query_total;
+    SqliteHelper sqliteHelper;
+    Cursor cursor;
+
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -67,10 +94,45 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//   @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
+
+    private void KasAdapter() {
+        aruskas.clear();
+        list_kas.setAdapter(null);
+
+        SQLiteDatabase database = sqliteHelper.getReadableDatabase();
+        cursor = database.rawQuery(query_kas, null);
+        cursor.moveToFirst();
+
+        int i;
+        for (i = 0; i < cursor.getCount(); i++) {
+            cursor.moveToPosition(i);
+
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("transaksi_id", cursor.getString(0));
+            map.put("status", cursor.getString(1));
+            map.put("jumlah", cursor.getString(2));
+            map.put("keterangan", cursor.getString(3));
+            map.put("tanggal", cursor.getString(5));
+            aruskas.add(map);
+        }
+        if (i == 0) {
+            Toast.makeText(getApplicationContext(), "Tidak Ada Transaksi Untuk Ditampilkan", Toast.LENGTH_LONG).show();
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, aruskas, R.layout.list_kas,
+                new String[]{"transaksi_id", "status", "jumlah", "keterangan", "tanggal"},
+                new int[]{R.id.text_transaksi_id, R.id.text_status, R.id.text_jumlah, R.id.text_keterangan});
+
+        list_kas.setAdapter(simpleAdapter);
+        list_kas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                transaksi_id = ((TextView) view.findViewById(R.id.text_transaksi_id)).getText().toString();
+//                ListMenu();
+            }
+        });
+
+//        KasTotal();
+
+    }
 }
